7fb69f5b34807eccdb947024f7f623680f0bfb72
Arhat161 <Arhat161@users.noreply.github.com>
2022-03-13 19:49:23 +0300

    After code formatting

--- src/ru/netology/Worker.java
+++ src/ru/netology/Worker.java
@@ -12,12 +12,11 @@ public class Worker {
 
     public void start() {
         for (int i = 0; i < 100; i++) {
-            if(i != 33) {
+            if (i != 33) {
                 callback.onDone("Task " + i + " is done");
             } else {
                 errorCallback.onError("Task " + i + " is error !");
             }
-
         }
     }
 
